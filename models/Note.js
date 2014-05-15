47c5817c133068497b1e109fd30d2530ba7c4a82
Tyler Morgan <tylermorgan86@gmail.com>
2014-05-15 11:31:13 -0700

    in class demo

---
+++ models/Note.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+
+var noteSchema = mongoose.Schema({
+  body: String
+});
+
+module.exports = mongoose.model('Note', noteSchema);
---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+var express  = require('express');
+var http     = require('http');
+var mongoose = require('mongoose');
+var noteRoutes = require('./routes/noteRoutes');
+
+var app = express();
+
+app.get('/api/v0_0_1/notes', noteRoutes.collection);
+
+mongoose.connect('mongodb://localhost/notes-development');
+
+app.set('port', process.env.PORT || 3000);
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+  console.log('server listening on port' + app.get('port'));
+});
